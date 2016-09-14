f2281921a30e0cafca63a3222668f44bfbd9f134
Jim Jagielski <jimjag@gmail.com>
2016-09-14 11:17:37 -0400

    prep for 4.12.3

--- winnt/microsft/config.h
+++ winnt/microsft/config.h
@@ -27,7 +27,7 @@
 
 /* Name and version number of this package */
 #define PACKAGE "dmake"
-#define VERSION "4.12"
+#define VERSION "4.12.3"
 #define BUILDINFO "Windows / MS Visual C++"
 
 #if defined (_MSC_VER)
